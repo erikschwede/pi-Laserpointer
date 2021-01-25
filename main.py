395b0bb43086c6b377caaaf4e22ef7534c7e00be
erikschwede <schwede.erik@gamil.com>
2021-01-25 16:53:32 +0000

    Changed main.py to read the json string

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
-	return 'Hello world!'
+	json_data = request.json
+	X_Value = json_data["X"]
+	Y_Value = json_data["Y"]
+	On_Value = json_data["ON"]
+	return "X_Value: {} Y_Value: {} On_Value: {}".format(X_Value, Y_Value, On_Value)
 
 if __name__ == '__main__':
 	app.run(debug=True, port=8888, host='0.0.0.0')
