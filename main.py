04c85e1bd7d64d93fdfc4b3d05817d93dbde5cc6
erikschwede <schwede.erik@gamil.com>
2021-01-28 10:48:33 +0000

    call controls.py on POST message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+import controls
 
 app = Flask(__name__)
 
@@ -9,7 +10,10 @@ def index():
 	X_Value = json_data["X"]
 	Y_Value = json_data["Y"]
 	On_Value = json_data["ON"]
-	return "X_Value: {} Y_Value: {} On_Value: {}".format(X_Value, Y_Value, On_Value)
+	controls.moveTo(X_Value, Y_Value, On_Value)
+	return "success!"
 
 if __name__ == '__main__':
+	controls.init_setup()
 	app.run(debug=True, port=8888, host='0.0.0.0')
+	controls.cleanup()
